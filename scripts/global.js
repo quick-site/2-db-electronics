e6f0367e5e06823c0b26694ef2ec1dc0b61ef835
rudipsq <132471589+rudipsq@users.noreply.github.com>
2024-06-17 11:59:45 +0200

    updates
    added getJson()
    added getLanguage()

--- scripts/global.js
+++ scripts/global.js
@@ -1,3 +1,4 @@
+//* - - language
 function switchLanguage(languageKey = "en") {
   const currentLocation = window.location.pathname
     .split("/")
@@ -5,4 +6,20 @@ function switchLanguage(languageKey = "en") {
     .join("/");
 
   window.location.href = "/" + languageKey + "/" + currentLocation;
+}
+
+function getLanguage() {
+  return window.location.href.split("/")[3];
+}
+
+//* - - data
+async function getJson(fileName) {
+  try {
+    let response = await fetch("../data/" + fileName);
+    let jsonData = await response.json();
+    return jsonData;
+  } catch (error) {
+    console.error("Error fetching or parsing JSON:", error);
+    return null;
+  }
 }
